4e5e44c51c245b6bd8cf010932e6fa7f489b85af
Dupuy Arnaud <arnaudupuy62@gmail.com>
2016-09-14 15:40:48 +0200

    Import Firebase and change project architecture

---
+++ Social/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Arnaud Dupuy on 14/09/2016.
+//  Copyright © 2016 Arnaud Dupuy. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
